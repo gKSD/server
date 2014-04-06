51f6bf91b9405022cac7cb493c281ce114be048d
Sofia Kiseleva <sofia-kiseleva@mail.ru>
2014-04-06 10:59:10 +0400

    folder test, and first default test

---
+++ test/FrontendImplTest.java
@@ -0,0 +1,22 @@
+package frontend;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+/**
+ * Created by sofia on 06.04.14.
+ */
+public class FrontendImplTest {
+    @org.testng.annotations.BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+}
